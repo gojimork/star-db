1ce47140a338aa48cb18250ca32563e382461f3d
gojimor <jgthjgth@gmail.com>
2022-12-23 22:57:11 +0300

    add component Spinner

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React from "react";
 
-import Header from '../header';
-import RandomPlanet from '../random-planet';
-import ItemList from '../item-list';
-import PersonDetails from '../person-details';
+import Header from "../header";
+import RandomPlanet from "../random-planet";
+import ItemList from "../item-list";
+import PersonDetails from "../person-details";
+import Spinner from "../spinner";
 
-import './app.css';
+import "./app.css";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Spinner />
       <RandomPlanet />
 
       <div className="row mb2">
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,13 @@
+import "./spinner.css";
+
+const Spinner = () => {
+  return (
+    <div class="loadingio-spinner-eclipse-nduf1g2beej">
+      <div class="ldio-29glc0h7uv4">
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
